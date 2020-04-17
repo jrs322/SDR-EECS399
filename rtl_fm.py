de093f8e6a98cace216b2571939aa7ea402df132
jrs322 <jrs322@case.edu>
2020-04-17 11:10:29 -0400

    Working version of rtl_fm.py with kill command added

--- rtl_fm.py
+++ rtl_fm.py
@@ -1,13 +1,20 @@
 import subprocess
-def rtl_fm_call(fm_type, frequency, sample_rate, gain="0", squelch="0",
+import time
+
+def rtl_fm_call(fm_type, frequency, sample_rate, output_rate, gain="0", squelch="0",
 		approx=None, E=None ):
     args_list = ["rtl_fm", "-f "+frequency,"-M "+fm_type,
-		"-s "+sample_rate, "-g "+gain,
+		"-s "+sample_rate, "-r "+output_rate, "-g "+gain,
 		"-l "+squelch]
     if E != None:
         args_list.append("-E "+E)
     if approx != None:
         args_list.append("-A "+approx)
-    ps = subprocess.Popen(args_list, stdout=subprocess.PIPE, stdin= subprocess.PIPE)
+    ps = subprocess.Popen(args_list)
+    try:
+        while True:
+            time.sleep(1)
+    finally:
+        ps.kill()
 
-rtl_fm_call(fm_type="wbfm",frequency="89.1M", sample_rate="12k")
+rtl_fm_call(fm_type="wbfm",frequency="89.1M", sample_rate="170k",output_rate="32k", E="s")
